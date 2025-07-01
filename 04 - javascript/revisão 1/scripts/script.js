0cf48708f038abaa649125bddb9db04a8cd7c041
VSRohodTeacher <143844089+VSRohodTeacher@users.noreply.github.com>
2025-07-01 12:03:05 -0300

    revisao

--- 04 - javascript/revisão 1/scripts/script.js
+++ 04 - javascript/revisão 1/scripts/script.js
@@ -15,6 +15,25 @@ function ex1() {
 // Terra e Uol. Quando escolhermos uma opção será acessado 
 // a página da opção escolhida.
 function ex2(){
+    let link = document.querySelector("#links").value;
 
+    location = link; 
 }
+document.querySelector("#links").addEventListener('change',ex2);
 
+//4) Monte um script conforme o layout apresentado abaixo contendo duas 
+// caixa de texto. Quando sair da caixa de percentual efetuar o 
+// cálculo de uma percentual sobre o valor digitado mostrando em uma caixa 
+// de alerta.
+
+// arrow function -> exercicio 4 - percentual do valor
+document.querySelector("#perc").addEventListener('focusout', () => {
+    // Nan - Not a Number
+    const valor = Number(document.querySelector("#valor").value);
+    const perc = Number(document.querySelector("#perc").value);
+
+    let resultado = valor * (perc / 100);
+
+    // toFixed() limita as casas decimais
+    alert(`O percentual do valor é ${resultado.toFixed(2)}`);
+});
