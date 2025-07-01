fbbf27d98767ee6ff75c5b5b2f95791b73f97f00
VSRohodTeacher <143844089+VSRohodTeacher@users.noreply.github.com>
2025-07-01 10:18:14 -0300

    revisão

--- 04 - javascript/aula 4 - dom/scripts/script.js
+++ 04 - javascript/aula 4 - dom/scripts/script.js
@@ -1,9 +1,14 @@
+// JS DE CÓDIGOS GERAIS
+
+// FORMAS ALTERNATIVAS DE PUXAR UM ELEMENTO
+
 // document.getElementById("#nome");
 // document.getElementsByClassName("caixa");
 // document.querySelector(".caixa");
 // document.getElementsByTagName();
 document.querySelector("#nome").value = "Victor";
 
+// FUNÇÃO PARA COLETAR O NOME DE UM INPUT
 function pegaNome(){
     nome = document.querySelector("#nome").value;
     alert(`Seja bem vindo :  ${nome}`);
@@ -15,10 +20,12 @@ function pegaNome(){
 // UTILIZADA NOVAMENTE / ARROW FUNCTION
 divSaiu = document.querySelector("#saiu");
 
+// ADICIONAR UM EVENTO DE TIRAR O MOUSE EM UMA DIV E ALTERAR SEU TEXTO
 divSaiu.addEventListener('mouseout',() => {
     divSaiu.innerHTML = `voce saiu do quadrado!`;
 })
 
+// ADICIONAR UM EVENTO DE COLOCAR O MOUSE EM UMA DIV E ALTERAR SEU TEXTO
 function entrouQuadrado(){
     divSaiu.innerHTML = `
         <p>voce entrou no quadrado!</p>
@@ -28,6 +35,34 @@ function entrouQuadrado(){
 divSaiu.addEventListener('mouseenter', entrouQuadrado);
 
 
+// FUNÇÃO PARA CLICAR EM UMA DIV E ALTERAR SEU DISPLAY
+// FAZENDO COM QUE ELA SUMA PELO CSS
 document.querySelector("#propaganda").addEventListener('click', () => {
     document.querySelector("#propaganda").style.display = "none";
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 04 - javascript/revisão 1/scripts/script.js
@@ -0,0 +1,20 @@
+// 1)Monte um script conforme o layout apresentado abaixo contendo 
+// uma caixa de texto e um botão quando digitarmos um endereço da 
+// internet e clicarmos no botão “Ir” será aberto uma
+// nova página acessando o endereço digitado.
+function ex1() {
+    const enderecoInput = document.querySelector('#enderecoInput');
+
+    let link = "https://" + enderecoInput.value;
+
+    location = link;
+}
+
+// 2) Monte um script conforme o layout apresentado abaixo contendo 
+// uma caixa de listagem onde possui as seguintes opções: Globo, 
+// Terra e Uol. Quando escolhermos uma opção será acessado 
+// a página da opção escolhida.
+function ex2(){
+
+}
+
